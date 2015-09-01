390f4aada1bad27375c509c29bc2dccc7b927464
LeoInSF <guoliangye1989@gmail.com>
2015-08-31 23:01:45 -0700

    1.1 practice(gye)

---
+++ unique_string.py
@@ -0,0 +1,22 @@
+import sys
+def unique_string(input_string):
+	if not input_string:
+		return 
+	else:    
+		str_list = list(input_string)
+		unique_char = set(str_list)
+		if len(str_list) == len(unique_char):
+		    return True
+		else:
+		    return False
+
+if __name__ == "__main__":
+	s = sys.argv[1]
+	result = unique_string(s)
+	if result:
+	    print "Unique string: " + s
+	elif result is None:
+	    print "Empty string, please provide valid string for validification" 
+	else:
+	    print "Duplicated string: " + s
+
