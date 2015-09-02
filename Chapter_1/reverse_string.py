af8c304f7066698375e8e5a8a3a7a603f4af6377
LeoInSF <guoliangye1989@gmail.com>
2015-09-01 22:51:00 -0700

    1.2 practice(gye)

---
+++ Chapter_1/reverse_string.py
@@ -0,0 +1,18 @@
+import sys
+
+def reverse_string(input_str):
+	"""
+	Reverse the input string and return it
+	"""
+	return input_str[::-1]
+
+if __name__ == "__main__":
+	
+	if len(sys.argv) != 2:
+		print "Usage: python reverse_string.py <test_string>"
+		exit(-1)
+	
+	s = sys.argv[1]
+	reversed_str = reverse_string(s)
+	print "Reversed string is: " + reversed_str
+
