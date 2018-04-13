103700800c454f55e1d702f1db1feafa215cd8a6
guoliangye1989@gmail.com <Guoliang_Ye@cable.comcast.com>
2018-04-13 16:55:22 -0700

    mine solution

---
+++ Chapter_1/mine_solution/string_compression.py
@@ -0,0 +1,41 @@
+import unittest
+
+def string_compression(string):
+    tmp_str = ''
+    pre_char = ''
+    char_count = 0
+
+    for char in string:
+        if pre_char == '':
+            pre_char = char
+            char_count += 1
+            continue
+
+        elif pre_char == char:
+            char_count += 1
+            continue
+
+        else:
+            tmp_str += pre_char + str(char_count)
+            pre_char = char
+            char_count = 1
+
+    tmp_str += pre_char + str(char_count)
+
+    if len(tmp_str) < len(string):
+        return tmp_str
+    else:
+        return string
+
+
+class Test(unittest.TestCase):
+
+    def test_string_compression(self):
+        test_data = [('aabcccccaaa', 'a2b1c5a3'),
+                        ('aabbccdd', 'aabbccdd')]
+
+        for [input, expected] in test_data:
+            self.assertEqual(string_compression(input), expected)
+
+if __name__ == '__main__':
+    unittest.main()
